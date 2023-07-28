a7616a73bb308bd6bc56d62bd0ecf9d38a0aaa11
Rafael Badaro <rafabadaro.15@gmail.com>
2023-07-27 21:32:48 -0300

    Initial Commit

---
+++ JaVi/JaViApp.swift
@@ -0,0 +1,17 @@
+//
+//  JaViApp.swift
+//  JaVi
+//
+//  Created by Rafael Badaró on 27/07/23.
+//
+
+import SwiftUI
+
+@main
+struct JaViApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
